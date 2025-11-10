b45b5fa3668c6a7ef71576a376c4bb2b5b2c9a7c
tuyendd <tuyendd.vpb@gmail.com>
2025-11-10 22:07:07 +0700

    build: update website dist

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',  // nếu bạn deploy ở root domain
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
